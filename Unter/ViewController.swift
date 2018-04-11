3c26e0842905b2848f12e59cdd79f6092e33cb75
Admin <admin@Mac-Admin.local>
2018-04-11 23:19:21 +1000

    Initial Commit

---
+++ Unter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Unter
+//
+//  Created by Admin on 11/4/18.
+//  Copyright © 2018 Brandon Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
