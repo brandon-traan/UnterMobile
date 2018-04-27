b9b0eef8f694b7cc580b6c7ec7b0460d3c4287bb
Brandon Tran <s3506901@b01005055-02.local>
2018-04-27 19:29:52 +1000

    Fix Photo Button Crash

--- Unter/Controller/SignUpViewController.swift
+++ Unter/Controller/SignUpViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class SignUpViewController: UIViewController {
     
 
+    // Page 1
     @IBOutlet weak var firstnameTextField: UITextField!
     @IBOutlet weak var lastnameTextField: UITextField!
     @IBOutlet weak var countryTextField: UITextField!
@@ -19,6 +20,7 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var continueButton: UIButton!
     
+    // Page 2
     @IBOutlet weak var googleDriveButton: UIButton!
     @IBOutlet weak var iCloudDriveButton: UIButton!
     @IBOutlet weak var dropboxButton: UIButton!
@@ -40,9 +42,6 @@ class SignUpViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func Button(_ sender: Any) {
-        attachmentLabel.text = "hello"
-    }
     
 
 
